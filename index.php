0a269ccd81c68c3545a894602d9882fe585a8523
Maxwell619 <maxwell.gatua@strathmore.edu>
2024-06-11 11:22:45 +0300

    changed html file to php

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="cssfile/style.css">
+    <link rel="icon" href="images/GTAV-LSCustoms-Logo.webp" > 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
+    <?php include_once("templates/indexnav.php");?>
+
+ 
+    <h1>LOS SANTOS CUSTOMS</h1>
+   
+        <style>
+            
+            .container {
+                
+                align-items:center;
+                flex-direction: row;
+                justify-content: center;
+                display: flex;
+            }
+           img{
+            width: 300px;
+                margin-left: 200px;
+                height: auto;
+                max-width: 100%;
+              
+           }
+            h1{
+                margin-left: 400px;
+                color:blue;
+                
+            }
+            a{
+                font-size: larger;
+                float:right;
+                
+            }
+        
+       
+        
+        </style>
+    
+      <div class="container">
+        <div class="images">
+            <img src="images/GTAV-LSCustoms-Logo.webp" alt="" style="font-size: larger;"> 
+        </div>
+      
+    </div>
+    
+        
+    
+    <p>
+        GREATEST MECHANICS IN AMERICA<br>
+    </p>
+   
+    <style>
+        p{
+            font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+            font-size: xx-large;
+            color: red;
+            margin-left: 500px;
+            
+        }
+        a{
+            color:black;
+    text-decoration: none;
+    float: left;
+    display: block;
+    padding: 14px 16px;
+    text-align: left;
+    border: rgb(249, 245, 245);
+    font-weight: bold;
+    
+    
+        }
+        .top_nav a:hover{
+    color: #faf5f5d0;
+    background-color: rgb(46, 11, 244);
+}
+.top_nav-right{
+    float: left;
+    border: rgb(247, 243, 243);
+    margin-top: 0px;
+}
+.top_nav-right a:hover{
+    color: #f7f0f0d0;
+    background-color: rgb(18, 11, 232);
+}
+ .top_nav a {
+    color: black;
+    font-weight: bold;
+    display: block;
+ }  
+ .img{
+    background-color: black;
+
+ }   
+
+ 
+        
+    </style>
+    
+   <div class="img">
+    <img src="./images/VIEW_01-2.jpg" alt="Background Image" style="position:fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: -1;margin-left: 0px;">
+    </div>
+
+
+
+
+
+
+</body>
+
+</html>

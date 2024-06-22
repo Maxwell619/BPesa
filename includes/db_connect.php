8b3a703e13a3435b06a4efa84a432b69518e2e18
Maxwell619 <maxwell.gatua@strathmore.edu>
2024-06-23 00:04:35 +0300

    declared variables

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -4,8 +4,13 @@ $username = "root";
 $password = "";
 $db_name="accountdetails";
 
+$fullname="fullname";
+$Reenter_password="Reenter_password";
+$email_address="email_address";
+$UserID="UserID";
+
 // Create connection
-$conn = new mysqli($servername, $username, $password,$db_name);
+$conn = new mysqli($servername, $username, $password ,$db_name);
 
 // Check connection
 if ($conn->connect_error) {

935a0b65ebe240f678e452a10727c005ffe96e16
Maxwell619 <maxwell.gatua@strathmore.edu>
2024-06-23 00:05:21 +0300

    imported table from xampp

---
+++ sql/db.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 18, 2024 at 09:04 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `accountdetails`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `newaccount`
+--
+
+CREATE TABLE `newaccount` (
+  `userID` varchar(50) NOT NULL,
+  `fullname` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `Reenter_password` varchar(50) NOT NULL,
+  `email_address` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `newaccount`
+--
+
+INSERT INTO `newaccount` (`userID`, `fullname`, `password`, `Reenter_password`, `email_address`) VALUES
+('', '', '', '', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `newaccount`
+--
+ALTER TABLE `newaccount`
+  ADD PRIMARY KEY (`userID`),
+  ADD UNIQUE KEY `Email` (`email_address`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

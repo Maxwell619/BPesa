1b9846231ec8c2f0e47c8d82501318d2cc64af2a
Maxwell619 <maxwell.gatua@strathmore.edu>
2024-07-06 00:54:19 +0300

    created edit account file

--- editnewaccount.php
+++ editnewaccount.php
@@ -1,59 +1,60 @@
+<link rel="stylesheet" href="cssfile/style.css">
 <?php 
-    require_once("includes/db_connect.php");
+require_once("includes/db_connect.php");
+ include_once("templates/nav.php");
 
+if(isset($_GET["userID"])) {
+    $userID = $_GET["userID"];
 
-$UserID = $_GET["UserID"];
+    // Fetch the account data
+    $spot_account = "SELECT * FROM newaccount WHERE userID = '$userID' LIMIT 1";
+    $spot_acc_res = $conn->query($spot_account);
 
-$spot_message = "SELECT * FROM newaccount WHERE UserID = '$UserID' LIMIT 1";
-$spot_mes_res = $conn->query($spot_message);
-
-$spot_mes_row = $spot_mes_res->fetch_assoc();
-
-if(isset($_POST["update_account"])){
-    $fullname = mysqli_real_escape_string($conn, addslashes($_POST["fullname"]));
-    $password = mysqli_real_escape_string($conn, addslashes($_POST["password"]));
-    $Reenter_password = mysqli_real_escape_string($conn, addslashes($_POST["Reenter_password"]));
-    $email_address = mysqli_real_escape_string($conn, ($_POST["email_address"]));
-
-
-    $update_message = "UPDATE newaccount SET fullname='$fullname', password='$password', Reenter_password='$Reenter_password', email_address='$email_address' WHERE UserID='$UserID' LIMIT 1";
-
-    if ($conn->query($update_account) === TRUE) {
-    header("Location: view_account.php");
+    if ($spot_acc_res && $spot_acc_res->num_rows > 0) {
+        $spot_acc_row = $spot_acc_res->fetch_assoc();
     } else {
-      echo "Error updating record: " . $conn->error;
+        echo "Account not found";
+        exit();
     }
 }
-    
+
+if(isset($_POST["update_account"])){
+    $fullname = mysqli_real_escape_string($conn, $_POST["fullname"]);
+    $password = mysqli_real_escape_string($conn, $_POST["password"]);
+    $Reenter_password = mysqli_real_escape_string($conn, $_POST["Reenter_password"]);
+    $email_address = mysqli_real_escape_string($conn, $_POST["email_address"]);
+    $userID = mysqli_real_escape_string($conn, $_POST["userID"]);
+
+    // Update the account data
+    $update_account = "UPDATE newaccount SET fullname='$fullname', email_address='$email_address', password='$password', Reenter_password='$Reenter_password' WHERE userID='$userID' LIMIT 1";
+
+    if ($conn->query($update_account) === TRUE) {
+        // Redirect back to view_account.php after updating
+        header("Location: view_account.php");
+        exit();
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
+}
 ?>
- <div class="ch">
-        <h1>CREATE AN ACCOUNT</h1>
-    </div>
-</head>
-<body>
-    <!-- Form to create an account -->
-    <div class="feedback">
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" autocomplete="off" class="feedback">
-          
-            <label for="fullname">Fullname:</label>
-            <input type="text" name="fullname" placeholder="Fullname" required value="<?php print $spot_mes_row["fullname"];?>"><br><br>
-            <label for="userpass">Password:</label>
-            <input type="password" name="password" placeholder="password" required value="<?php print $spot_mes_row["password"];?>"><br><br>
-            <label for="userpass">Re-enter password:</label>
-            <input type="password" name="Reenter_password" placeholder="password" required value="<?php print $spot_mes_row["Reenter_password"];?>"><br><br>
-            <label for="email_address">Email address:</label>
-            <input type="email" name="email_address" placeholder="Email Address" required value="<?php print $spot_mes_row["email_address"];?>"><br><br>
-            <!-- Checking if the user has read terms and conditions -->
-            <h4>Have you read the terms and conditions?</h4>
-            <div>
-                <label>Yes</label>
-                <input type="radio" name="rate" value="1" id="Yes"><br><br>
-            </div>
-            <div>
-                <label>No</label>
-                <input type="radio" name="rate" value="2" id="No"><br><br>
-            </div>
-            <input type="submit" value="Update Account" style="background-color: aqua;" name="update_account">
-            <input type="hidden" name="UserID" value="<?php print $spot_mes_row["UserID"];?>">
+
+<!-- HTML form for editing account data -->
+<link rel="stylesheet" href="cssfile/style.css">
+<div class="top_nav">
+<?php include_once("templates/nav.php");?>
+
+<div class="edit">
+
+    <form action="editnewaccount.php" method="post">
+        <input type="hidden" name="userID" value="<?php echo isset($spot_acc_row['userID']) ? htmlspecialchars($spot_acc_row['userID']) : ''; ?>">
+        <label for="fullname">Full Name:</label>
+        <input type="text" id="fullname" name="fullname" value="<?php echo isset($spot_acc_row['fullname']) ? htmlspecialchars($spot_acc_row['fullname']) : ''; ?>" required><br><br>
+        <label for="password">Password:</label>
+        <input type="text" id="password" name="password" value="<?php echo isset($spot_acc_row['password']) ? htmlspecialchars($spot_acc_row['password']) : ''; ?>" required><br><br>
+        <label for="Reenter_password">Re-enter Password:</label>
+        <input type="text" id="Reenter_password" name="Reenter_password" value="<?php echo isset($spot_acc_row['Reenter_password']) ? htmlspecialchars($spot_acc_row['Reenter_password']) : ''; ?>" required><br><br>
+        <label for="email_address">Email Address:</label>
+        <input type="email" id="email_address" name="email_address" value="<?php echo isset($spot_acc_row['email_address']) ? htmlspecialchars($spot_acc_row['email_address']) : ''; ?>" required><br><br>
+        <button type="submit" name="update_account" style="background-color: aqua;" >Update</button>
     </form>
-        </form>
+</div>

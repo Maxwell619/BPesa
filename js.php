3b16241bacaf557b8051d7a98c7d4f5e8e8d6b51
Maxwell619 <maxwell.gatua@strathmore.edu>
2024-07-06 00:54:33 +0300

    created new file

---
+++ js.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body onload startTime>
+    <p id="demo"></p>
+    <script>
+        document.getElementById('demo').innerHTML="This is my first JS code";
+    </script><br><br>
+<button type="button" onclick="document.getElementById('MyImg').src='images/amg-gt-exterior-right-front-three-quarter-60800.webp'>
+    <img src="images/amg-gt-exterior-right-front-three-quarter-60800.webp" alt="" style="width: 300px;" id="MyImg">
+
+
+
+<script>
+    function myFnc(){
+        document.getElementById('Tue').innerHTML="Set a paragraph here";
+    }
+</script>
+
+<?php date_default_timezone_set("Africa/Nairobi")?>
+Static Timer:<?php print date("H:i:s");?>
+<br>
+Dynamic Time: <span id="tmr"></span>
+<br><br>
+<script>
+    window.alert('You cannot delete this user!!');
+    </script>
+<script src="js/"async defer></script>
+
+<script>
+    document.write(5*6);
+    <br><br>
+</script>
+<a href="" onclick="return confirm"
+<script>
+  console.log(25+85);
+</script>
+</body>
+</html>
